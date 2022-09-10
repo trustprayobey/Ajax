7bb6a869b9e213c08da0d60029e5367e8bea3b2b
trustprayobey <brishadow24@gmail.com>
2022-09-10 21:38:08 +0300

    Intro

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 console.log('Time to put the xml in ECMAScript')
+
+//What is Ajax?
+//Async Js and Xml
+//Set of web technologies
+//Send and receive data asynchronously
+//Does not intefere with the current web page
+//JSON has replaced XML for the most part
+
+
 //xml http request (XHR Object)
 
 //api in the form of an object
@@ -6,5 +15,22 @@ console.log('Time to put the xml in ECMAScript')
 //Methods transfer data between client/server
 //Can be used with other protocols than HTTP
 //Can work with data other than XML(JSON.plain text)
+
+//Ajax call is made with
+//first is using the XHR
+//Libraries and other methods used to make calls are
+/*
+jQuery
+Axios
+Superagent
+FetchAPI(part of the browser)
+Prototype
+Node HTTP(using node js)
+*/
+
+
+
+
+
 
 
