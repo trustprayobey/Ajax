5fe7808363d8e6da2e7da03bd888aae9217ff9b5
trustprayobey <brishadow24@gmail.com>
2022-09-10 21:31:09 +0300

    So it is asynchronous javasvtipt and xml

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('Time to put the xml in ECMAScript')
+//xml http request (XHR Object)
+
+//api in the form of an object
+//Provided by the browser's js environment
+//Methods transfer data between client/server
+//Can be used with other protocols than HTTP
+//Can work with data other than XML(JSON.plain text)
+
+
