bba6fb28d116b26b3ce5eb7b12e56831da06d704
trustprayobey <brishadow24@gmail.com>
2022-09-11 15:23:53 +0300

    on ready state

--- index.js
+++ index.js
@@ -36,11 +36,27 @@ function loadText(){
     //OPEN type url asyn or not
     xhr.open('GET','sample.txt',true)
 
-    xhr.onload = function(){
-        if(xhr.status == 200){
+    // xhr.onload = function(){
+    //     if(xhr.status == 200){
+    //         console.log(this.responseText)
+    //     }
+    // }
+
+    //Instead of using the onload
+    xhr.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
             console.log(this.responseText)
         }
     }
+
+    //ready state values are:
+    //0 - request not intialized
+    //1 - server connection established
+    //2 - request received
+    //3 - processing request
+    //4 - request finished and response is ready
+
+
     //sends request
     xhr.send()
 
